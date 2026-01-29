0b0a9326e7e7ca7f984d91ca9ac696ab9199f1cd
TQ <tianqiz12704@gmail.com>
2026-01-29 01:00:25 -0800

    fix shape typo, add test notebook

--- sleep_tester.py
+++ sleep_tester.py
@@ -138,7 +138,7 @@ def main() -> None:
     print(sleep_sampler.n_phases)
     
     print(len(sleep_sampler.dataset_indices))
-    print(shape(np.array(sleep_sampler.folds)))
+    print(np.array(sleep_sampler.folds).shape)
 
 if __name__ == "__main__":
     main()
