4917fd5c367847532479b17dc60aeeffc0a49040
TQ <tianqiz12704@gmail.com>
2026-01-29 00:32:14 -0800

    fix outdated hf hub call in trainer

--- src/trainer.py
+++ src/trainer.py
@@ -12,7 +12,7 @@ import torch
 import torch.distributed as dist
 import torch.nn as nn
 from huggingface_hub.hf_api import create_repo
-from huggingface_hub.repository import Repository
+# from huggingface_hub.repository import Repository
 from huggingface_hub.utils._errors import HfHubHTTPError
 from omegaconf import OmegaConf
 
