896e4f22386fbffd6c1c720f2a2404df7b3b0d09
TQ <tianqiz12704@gmail.com>
2026-02-19 17:28:06 -0800

    remove debugging print statement

--- src/dataloader.py
+++ src/dataloader.py
@@ -182,7 +182,7 @@ class _CustomSingleProcessDataLoaderIter(_BaseDataLoaderIter):
             data["input_ids"] = input_ids
 
         # remove ignored columns
-        print(f"Ignore columns: {self.loader.ignore_columns}")
+        # print(f"Ignore columns: {self.loader.ignore_columns}")
         if self.loader.ignore_columns is not None:
             for ignore_column in self.loader.ignore_columns:
                 data.pop(ignore_column, None)
