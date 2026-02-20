cb860b3a52e67e7e5663ba3f148cde6ef93bc1da
TQ <tianqiz12704@gmail.com>
2026-02-19 17:21:13 -0800

    absorb unknown kwargs into torch_mask_tokens

--- src/objective_curriculum/units/mlm.py
+++ src/objective_curriculum/units/mlm.py
@@ -47,7 +47,7 @@ class _DataCollatorForLanguageModeling(DataCollatorForLanguageModeling):
         return batch
 
     # We override this function to allow us to adjust the probability of unmasking
-    def torch_mask_tokens(self, inputs: Any, special_tokens_mask=None):
+    def torch_mask_tokens(self, inputs: Any, special_tokens_mask=None, **kwargs):
         """
         Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
         """
