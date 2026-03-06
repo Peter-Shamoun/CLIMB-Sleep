3a90edd71212b6ac2ab6f6f29b2f549bc9bbf7f1
TQ <tianqiz12704@gmail.com>
2026-03-06 12:49:35 -0800

    add logging statements to setup script

--- setup.sh
+++ setup.sh
@@ -8,11 +8,13 @@ pip install torch==2.9.1 torchvision==0.24.1 --index-url https://download.pytorc
 pip install hydra-core
 pip install wandb
 pip install -r requirements.txt
+echo "Dependencies installed"
 
 # Install evaluation pipeline
 git submodule update --init
 mkdir lib/evaluation-pipeline-2025/evaluation_data
 unzip $EVAL_DATA_DIR -d lib/evaluation-pipeline-2025/evaluation_data/
+echo "Eval pipeline installed"
 
 # Login
 wandb login
