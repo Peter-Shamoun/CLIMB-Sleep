578eb6ba5f95da6d4e5be363f230a504d2f29550
TQ <tianqiz12704@gmail.com>
2026-04-08 19:58:53 -0500

    Catch baseline_run up to current experiment standard

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 EVAL_DATA_DIR=$1
 
+# Create virtual environment
 python -m venv .venv
-source .venv/bin/activate
 
-# Install dependencies
-pip install torch==2.9.1 torchvision==0.24.1 --index-url https://download.pytorch.org/whl/cu126
-pip install hydra-core
-pip install wandb
-pip install -r requirements.txt
+# Install dependencies into environment
+.venv/bin/python -m pip install torch==2.9.1 torchvision==0.24.1 --index-url https://download.pytorch.org/whl/cu126
+.venv/bin/python -m pip install hydra-core
+.venv/bin/python -m pip install wandb
+.venv/bin/python -m pip install -r requirements.txt
+echo "Dependencies installed"
 
 # Install evaluation pipeline
 git submodule update --init
 mkdir lib/evaluation-pipeline-2025/evaluation_data
 unzip $EVAL_DATA_DIR -d lib/evaluation-pipeline-2025/evaluation_data/
+echo "Eval pipeline installed"
 
 # Login
-wandb login
+.venv/bin/wandb login
 
 echo "Completed setup"
