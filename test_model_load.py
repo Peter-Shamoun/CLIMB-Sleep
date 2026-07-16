ac53aacfc533048dcd3c585f0b808ad28c8ffe09
TQ <tianqiz12704@gmail.com>
2026-07-15 22:30:41 -0500

    Add hydra config to model load tester

--- test_model_load.py
+++ test_model_load.py
@@ -2,9 +2,21 @@ import logging
 import os
 import argparse
 import math
+
+import hydra
+from hydra.core.config_store import ConfigStore
+
 from src.config import BabyLMConfig
 
+# type-checks dynamic config file
+cs = ConfigStore.instance()
+cs.store(name="base_config", node=BabyLMConfig)
 
+# A logger for this file
+logger = logging.getLogger(__name__)
+
+@record
+@hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(cfg: BabyLMConfig):
     logger.info("Initializing model")
     model = load_base_model(cfg)
